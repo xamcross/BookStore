7797070f7943400581f9b787c673b71e819326f4
xamcross <xamcross@gmail.com>
2015-10-05 18:04:16 +0300

    added new libraries to support jstl changed main controller a bit

--- WebContent/WEB-INF/jsp/index.jsp
+++ WebContent/WEB-INF/jsp/index.jsp
@@ -1,31 +1,26 @@
 <%@taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-<%@page import="java.util.*"  %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="tag" uri="http://www.springframework.org/tags" %>
+<%@page import="java.util.*"%>
 
 <html>
 <head>
-    <title>Welcome to Black Books!</title>
+<title>Welcome to Black Books!</title>
 </head>
 <body>
-<h2><font size="24">Black Books</font></h2>
-<form:form method="GET" action="/HelloWeb/staticPage">
-<table>
-	<%
-		HashMap<String, String> bookTitles = (HashMap<String, String>) request.getAttribute("bookTitles");
-		for (int i=0; i<bookTitles.size(); i++){
-	%>
-	<tr>
-	   <td>
-	   <%= bookTitles.get(i+"") %>
-	   </td>
-    	<td>
-	    	<input type="submit" value="Add to cart"/>
-	    </td>
-    </tr>
-	<% 	
-		}
-	%>
-</table>  
-</form:form>
+	<h2>
+		<font size="24">Black Books</font>
+	</h2>
+	<form:form method="GET" action="/HelloWeb/staticPage">
+		<table>
+			<c:forEach items="${titles }" var="title">
+			<tr>
+				<td>${title}</td>
+				<td><input type="submit" value="Add to cart" /></td>
+			</tr>
+			</c:forEach>
+
+		</table>
+	</form:form>
 </body>
 </html>
--- src/xam/cross/WebController.java
+++ src/xam/cross/WebController.java
@@ -1,10 +1,7 @@
 package xam.cross;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -13,40 +10,34 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.sun.glass.ui.Application;
-
 @Controller
 public class WebController {
 
-   @RequestMapping(value = "/index", method = RequestMethod.GET)
-   public String index(ModelMap model, HttpServletRequest request, HttpServletResponse response) {
-		ApplicationContext context = 
-	             new ClassPathXmlApplicationContext("Beans.xml");
-		
-		Book book = (Book) context.getBean("book");
-		
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String index(ModelMap model) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+
 		ShoppingCart cart = (ShoppingCart) context.getBean("shoppingCart");
-		
-		Map<String, String> bookTitles = new HashMap<>();
-		
-		for (int i=0; i < cart.getNumberOfBooks(); i++){
-			bookTitles.put(i+"", cart.getBookByIndex(i).getTitle());
+
+		List<String> bookTitles = new ArrayList<>();
+
+		for (int i = 0; i < cart.getNumberOfBooks(); i++) {
+			bookTitles.add(cart.getBookByIndex(i).getTitle());
 		}
-		
-		request.setAttribute("bookTitles", bookTitles);
-		
-	   return "index";
-   }
-   
-   @RequestMapping(value = "/staticPage", method = RequestMethod.GET)
-   public String redirect1() {
-     
-      return "redirect:/pages/final.htm";
-   }
-   
-   public void showAllBooksInCart(ShoppingCart cart){
-	   for (int i=0; i < cart.getNumberOfBooks(); i++){
-		   cart.getBookByIndex(i).getTitle();
-	   }
-   }
+
+		model.addAttribute("titles", bookTitles);
+		return "index";
+	}
+
+	@RequestMapping(value = "/staticPage", method = RequestMethod.GET)
+	public String redirect1() {
+
+		return "redirect:/pages/final.htm";
+	}
+
+	public void showAllBooksInCart(ShoppingCart cart) {
+		for (int i = 0; i < cart.getNumberOfBooks(); i++) {
+			cart.getBookByIndex(i).getTitle();
+		}
+	}
 }
